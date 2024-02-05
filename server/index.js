d1151c12870771c3bb4c7d974cd7bf832f9e3247
nidhipawar5 <nidhipawar0504@gmail.com>
2024-02-05 19:53:25 +0530

    cors issue logging

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
+app.use((req, res, next) => {
+  console.log('Request received:', req.method, req.url);
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 app.get('/', async (req, res) => {
   res.status(200).json({
     message: 'Hello from DALL.E!',
